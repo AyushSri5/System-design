cd1b50c071ff699242bd4a461c6f21477e07d215
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-11-11 20:55:56 +0530

    Getters & Setters

--- oops/encapsulation/App.java
+++ oops/encapsulation/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
         Encapsulation ep= new Encapsulation(1,22);
-
+        System.out.println(ep.getter());
+        ep.setter(23);
+        System.out.println(ep.getter());
     }
 }
--- oops/encapsulation/Encapsulation.java
+++ oops/encapsulation/Encapsulation.java
@@ -6,4 +6,12 @@ public class Encapsulation {
         id=i;
         age=ag;
     }
+
+    public int getter(){
+        return age;
+    }
+
+    public void setter(int age){
+        this.age=age;
+    }
 }
