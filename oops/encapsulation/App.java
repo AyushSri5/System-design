08603c8d1cbe658fa1b5112a93f65ca4bc3413e4
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-11-11 20:43:14 +0530

    Encapsulation

---
+++ oops/encapsulation/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Encapsulation ep= new Encapsulation(1,22);
+
+    }
+}
---
+++ oops/encapsulation/Encapsulation.java
@@ -0,0 +1,9 @@
+public class Encapsulation {
+    private int id;
+    private int age;
+
+    public Encapsulation(int i,int ag){
+        id=i;
+        age=ag;
+    }
+}
