b38fcaa07e3fad1f853b1db084fec2ff5502ff2d
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2025-01-19 19:48:00 +0530

    Prototype-Pattern

--- Prototype-Pattern/Prototype.java
+++ Prototype-Pattern/Prototype.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public interface Prototype {
+public interface Prototype {
+    public Prototype clone();
 }
--- Prototype-Pattern/Student.java
+++ Prototype-Pattern/Student.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Student {
+public class Student implements Prototype{
+    private int age;
+    private String name;
+    private int rollNumber;
+
+    public Student(int age,String name,int rollNumber){
+        this.age=age;
+        this.name=name;
+        this.rollNumber=rollNumber;
+    }
+    @Override
+    public Prototype clone() {
+        return new Student(age,name,rollNumber);
+    }
 }
